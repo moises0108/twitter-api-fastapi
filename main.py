63f52e13a98fd38afee4f61fbe72f1dc5eb16001
Moises Patino <moisespatinoph@gmail.com>
2022-08-20 17:11:41 -0400

    path operation users

--- main.py
+++ main.py
@@ -1,65 +1,77 @@
 #Python
-from datetime import date,datetime
-from uuid import UUID
-from typing import Optional
+from typing import List
 #Pydantic
-from pydantic import BaseModel
-from pydantic import Field
-from pydantic import EmailStr
-from pydantic import ValidationError,validator
-#Fast API
+#FastApi
 from fastapi import FastAPI
+from fastapi import status
+#TwitterApi
+from models import Tweet,User,UserLogin
 
 app=FastAPI()
 
-
-#Models
-class UserBase(BaseModel):
-    user_id:UUID = Field(...)
-    email:EmailStr = Field(
-        ...,
-        example="moisespatinoh@gmail.com"
-    )
-class User(UserBase):
-    first_name:str = Field(
-        ...,
-        min_length=1,
-        max_length=50,
-        example="Moises"
-        )
-    last_name:str = Field(
-        ...,
-        min_length=1,
-        max_length=50,
-        example="Patino"
-        )
-    birth_date : Optional[date] = Field(default=None,example="2000-08-01")
-    
-    @validator('birth_date')
-    def is_adult(cls,birth_date):
-        today_date=date.today()
-        total_years=today_date-birth_date
-
-        if total_years.years<18:
-            raise ValueError("Must be over 18")
-        else:
-            return birth_date
-class UserLogin(UserBase):
-    password:str = Field(
-        ...,
-        min_length=8,
-        max_length=16,
-        example="Moises123"
-        )
-
-class Tweet(BaseModel):
-    id:UUID=Field(...)
-    by:User = Field(...)
-    content:str=Field(...,min_length=1,max_length=256)
-    created_at:datetime = Field(default=datetime.now())
-    update_now:Optional[datetime] = Field(default=None) 
-
-
+#PathOperation
 @app.get(path="/")
 def home():
     return {"twitter API":"Working"}
+
+##Users
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Users"]
+    )
+def signup():
+    pass
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a user",
+    tags=["Users"]
+    )
+def login():
+    pass
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all the users",
+    tags=["Users"]
+    )
+def show_all_users():
+    pass
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a User",
+    tags=["Users"]
+    )
+def show_a_user():
+    pass
+
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a User",
+    tags=["Users"]
+    )
+def delete_a_user():
+    pass
+
+@app.put(
+    path="/users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a User",
+    tags=["Users"]
+    )
+def update_a_user():
+    pass
+##Tweets
